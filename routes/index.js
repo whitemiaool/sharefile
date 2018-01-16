5bbbbc8d9c8e6c159dec5fcc24552e748cdf5b9c
dyxuan <441046777@qq.com>
2018-01-16 17:17:09 +0800

    last

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 // var fs = require('fs');
 
+var pt = '/home/dyxuan${pt}';
 var util = require('../util');
 
 
@@ -16,17 +17,17 @@ router.get('/read/*',function(req,res,next){
 
 router.get('/down/*',function(req,res,next){
 	let path = req.params[0];
-	res.download(`/${path}`)
+	res.download(`${pt}/${path}`)
 })
 
 router.get('/getFile', function (req, res, next) {
-	var file = util.readDir(`/share`);
+	var file = util.readDir(`${pt}`);
 	let allFile = [];
 	file&&file.forEach((v,i)=>{
 		let oneFile = {};
 		oneFile.name = v;
-		oneFile.path = '/share';
-		oneFile.type = util.isDir(`/share/${v}`)? 1:2;
+		oneFile.path = `${pt}`;
+		oneFile.type = util.isDir(`${pt}/${v}`)? 1:2;
 		allFile.push(oneFile);
 	})
 	// res.render('index', { title: 'Express' });
@@ -37,12 +38,12 @@ router.get('/getFile', function (req, res, next) {
 router.get('/getFile/*', function (req, res, next) {
 	let path = req.params[0];
 	console.log(`/${path}`);
-	if(util.isDir(`/${path}`)) {
-		var file = util.readDir(`/${path}`);
+	if(util.isDir(`${pt}/${path}`)) {
+		var file = util.readDir(`${pt}/${path}`);
 		console.log(util.fomrmatDir(file,path))
 		res.status(200).json({ file: util.fomrmatDir(file,path),type:1})
 	} else {
-		res.status(200).json({ file: util.readFile(`/${path}`),type:2})
+		res.status(200).json({ file: util.readFile(`${pt}/${path}`),type:2})
 		// res.download(`/${path}`)
 	}
 	
